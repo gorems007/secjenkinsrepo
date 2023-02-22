5d7b3f124574a5ef0673d0c4686f68650240a416
mahendra <gorems007@gmail.com>
2023-02-22 07:36:23 +0530

    adding clASS FILE

---
+++ Jobchain02.java
@@ -0,0 +1,9 @@
+
+public class Jobchain02 {
+
+	public static void main(String[] args) {
+		System.out.println("this is job chain 02");
+
+	}
+
+}
---
+++ Jobchain_01.java
@@ -0,0 +1,9 @@
+
+public class Jobchain_01 {
+
+	public static void main(String[] args) {
+		System.out.println(" this is jobchain 01");
+
+	}
+
+}
