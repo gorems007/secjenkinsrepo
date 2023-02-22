84a76ecedfc470e1786d2533fbeb23efa9232b90
mahendra <gorems007@gmail.com>
2023-02-22 07:57:38 +0530

    commited .java file

--- Jobchain02.java
+++ Jobchain02.java
@@ -2,7 +2,7 @@
 public class Jobchain02 {
 
 	public static void main(String[] args) {
-		System.out.println("this is job chain 02");
+		System.out.println("this is job chain 02 ");
 
 	}
 
--- Jobchain_01.java
+++ Jobchain_01.java
@@ -2,7 +2,7 @@
 public class Jobchain_01 {
 
 	public static void main(String[] args) {
-		System.out.println(" this is jobchain 01");
+		System.out.println(" this is jobchain 01 ");
 
 	}
 
